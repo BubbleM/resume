411c603c1913c4f5d2da0cd3e8c35c67275497a1
BubbleM <1870408473@qq.com>
2017-08-12 22:57:51 +0800

    add autoReload

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -9,6 +9,14 @@ function handleError(err) {
   this.emit('end');
 }
 
+gulp.task('browser-sync',['build', 'sass'], () => {
+  browserSync({
+    server: {
+      baseDir: '..' // 基地建在那个文件夹,即index.html等
+    }
+  });
+});
+
 gulp.task('build', () => {
   gulp.src('pages/*.html')
     .pipe(wrap({src: 'layout/default.html'}))
@@ -19,7 +27,8 @@ gulp.task('sass', () => {
   gulp.src('styles/main.scss')
     .pipe(sass()).on('error', handleError)
     .pipe(prefix())
-    .pipe(gulp.dest('../styles'));
+    .pipe(gulp.dest('../styles'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 /*gulp.task('cp', () => {
@@ -27,10 +36,14 @@ gulp.task('sass', () => {
     .pipe(gulp.dest('..'));
 });*/
 
+gulp.task('rebuild', ['build'], () => {
+  browserSync.reload(); // 浏览器自动刷新
+});
+
 gulp.task('watch', () => {
-  gulp.watch(['**/*.html'], ['build']);
+  gulp.watch(['**/*.html'], ['rebuild']);
   gulp.watch(['styles/*.scss'], ['sass']);
 });
 
-gulp.task('default', ['sass', 'build', 'watch']);
+gulp.task('default', ['browser-sync', 'watch']);
 
